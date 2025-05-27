ca81616d8e6c3a1d0cedfecaac2ae8fb6508f45f
Nam Dau <52932605+namdau14@users.noreply.github.com>
2025-05-26 19:06:00 -0700

    Delete Google Drive directory

--- Google Drive/README.md
+++
@@ -1,1 +0,0 @@
-Simple Extension to add a button to quickly access Google Drive from the panel
--- Google Drive/open.js
+++
@@ -1,5 +0,0 @@
-browser.spacesToolbar.addButton('GoogleDrive', {
-    title: "Google Drive",
-    defaultIcons: "drive.svg",
-    url: "https://drive.google.com/drive/u/0/home"
-});
